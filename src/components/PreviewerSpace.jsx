d0d034cafa9db36a1eb89cb33c848b4fc5a901c8
Mr. Sonuk5 <sonukr96710@gmail.com>
2026-01-17 20:59:41 +0530

    Remove Pasrshing function from PreviewerSpace
    Removed unnecessary Pasrshing function from PreviewerSpace.

--- src/components/PreviewerSpace.jsx
+++ src/components/PreviewerSpace.jsx
@@ -7,22 +7,6 @@ import PreviewerHeading from './PreviewerHeading';
 
 const PreviewerSpace = ({contant}) => {
   const mark = <Markdown>{contant}</Markdown>
-
-  const Pasrshing = () =>{
-    if(contant === '```'){
-      let str = '';
-      let i = 0;
-
-      while (contant !== '```'){
-        str.includes(contant);
-        i++;
-      }
-
-      return str;
-    }
-  }
-  
-  
   return (
     <>
       <div className={style.previewerSpace}>
